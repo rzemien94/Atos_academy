6a5c445df778727b23c011854029ff0555967241
rzemien94 <rzemieniewski94@gmail.com>
2022-06-04 16:01:35 +0200

    exercises

--- task6_2.py
+++ task6_2.py
@@ -12,11 +12,14 @@ while still_playing:
         temp = int(input("podaj liczbe 1-50: (-1 to exit)"))
     except ValueError as e:
         print("you should give an int value")
+        continue
     if temp == guess_num or temp == -1:
         print(f"congrats, you won after: {counter} moves!")
         break
     if temp > guess_num:
         print('to much')
+        continue
     else:
         print('not enough')
+        continue
 print('end of game')
